13a893fc6a8972c2c5cbe9c428b15d3dc5ac7dad
Chantel Punt <csky.punt@gmail.com>
2024-08-01 17:47:49 +1000

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import LeftSideBar from '@/components/LeftSideBar'
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div>
+      <main>
+        <LeftSideBar />
+        {/* main */}
+        {children}
+        <p className='text-white-1'>RIGHT SIDEBAR</p>
+      </main>
+    </div>
+  )
+}
